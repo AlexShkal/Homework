04a7a4a54f6c4e32d7b1a7deaec8999ba5c14309
Alex <99368083+AlexShkal@users.noreply.github.com>
2023-08-30 22:08:25 +0300

    Add files via upload

---
+++ lessons3.py
@@ -0,0 +1,29 @@
+# Найти самое длинное слово в веденном предложении.
+# Учтите что в предложении есть знаки препинания.
+
+str_ = input('Enter a string: ')
+
+def longest_word(value: str) -> str:
+    lst = value.split()
+    new_lst = [el.strip(",.\\:;/!?'\"") for el in lst]
+    return max(new_lst, key=len)
+
+# Вводится строка. Требуется удалить из нее повторяющиеся символы и все пробелы.
+# Например, если было введено "abc cde def", то должно быть выведено "abcdef".
+
+str_ = input('Enter a string: ')
+
+def only_unique_characters(value: str) -> str:
+    return ''.join(dict.fromkeys(value.replace(' ', '')))
+
+
+# Посчитать количество строчных (маленьких) и прописных (больших) букв в веденной строке.
+# Учитывать только английские буквы.
+
+str_ = input('Enter a string: ')
+
+def lowercase_and_uppercase_letters(value: str) -> (int, int):
+    upper = sum(1 for el in value if 65 <= ord(el) <= 91)
+    lower = sum(1 for el in value if 97 <= ord(el) <= 123)
+    return 'Number of uppercase letters = {} and lowercase letters = {}'.format(upper, lower)
+
---
+++ lessons6.py
@@ -0,0 +1,51 @@
+# Выведите n-ое число Фибоначчи, используя только временные переменные,
+# циклические операторы и условные операторы. n - вводится.
+
+n = int(input('Введите номер числа Фибоначчи '))
+
+
+def fibonacci_number(value: int):
+    first = 0
+    second = 1
+
+    if value == 1:
+        return print(f"Первое число Фибоначчи  {first}")
+
+    elif value == 2:
+        return print(f"Второе число Фибоначчи  {second}")
+
+    for _ in range(2, value):
+        temp = second
+        second = first + second
+        first = temp
+    return print(f" {value} число Фибоначчи  {second}")
+
+# Определите, является ли число палиндромом (читается слева направо и справа налево одинаково).
+# Число положительное целое, произвольной длины.
+# Задача требует работать только с числами (без конвертации числа в строку или что-нибудь еще).
+n = int(input())
+n1 = n
+m = 0
+while n != 0:
+    i = n % 10
+    m = m * 10 + i
+    n = n // 10
+if m == n1:
+    print('Полиндром')
+else:
+    print('Не является полиндромом')
+
+# Напишите программу, которая печатает цифры от 1 до 100, но вместо чисел, кратных 3 пишет Fizz, вместо чисел кратный 5 пишет Buzz,
+# а вместо чисел одновременно кратных и 3 и 5 - FizzBuzz.
+for num in range(1, 101):
+    fizz = num % 3 == 0
+    buzz = num % 5 == 0
+
+    if fizz and buzz:
+        print("FizzBuzz")
+    elif fizz:
+        print("Fizz")
+    elif buzz:
+        print("Buzz")
+    else:
+        print(num)
